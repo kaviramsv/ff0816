a2452826581977facb27e397f921839d80429b29
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-03-03 12:33:15 -0500

    Add enter key press to stop i/p emitting logic

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { FormControl, FilledInput } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { SocketContext } from '../../context/socket';
+import UserAvatar from '../Sidebar/BadgeAvatar';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -18,11 +20,37 @@ const useStyles = makeStyles(() => ({
 const Input = ({ otherUser, conversationId, user, postMessage }) => {
   const classes = useStyles();
   const [text, setText] = useState('');
-
+  const socket = useContext(SocketContext);
+  
+  const sendTyping = () => {
+    console.log("in send typing Input",conversationId,otherUser.id,user.id);
+    socket.emit("typing", {
+      conversationId: conversationId,
+      recipientId: otherUser.id,
+      senderId: user.id,  
+    });
+  }
+  const sendStoppedTyping = () => {
+    console.log("in send stop typing Input",conversationId,otherUser.id,user.id);
+    socket.emit("stop", {
+      conversationId: conversationId,
+      recipientId: otherUser.id,
+      senderId: user.id,  
+    });
+  }
   const handleChange = (event) => {
-    setText(event.target.value);
+    setText(event.target.value);    
+    if (event.target.value==="") {      
+      sendStoppedTyping();
+    }else{
+      sendTyping();
+    }
   };
-
+  const handleKeyPress = (event) => {
+    if(event.key === 'Enter'){      
+      sendStoppedTyping();
+    }
+  }
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
@@ -38,7 +66,20 @@ const Input = ({ otherUser, conversationId, user, postMessage }) => {
     await postMessage(reqBody);
     setText('');
   };
+ 
 
+  // const addTyping = useCallback((data) => {
+  //   console.log("data tying ",data);
+  
+  // }, []);
+  // useEffect(() => {
+  //   // Socket init
+  //   socket.on("typing-success", addTyping);   
+
+  //   return () => {    
+  //     socket.off("typing-success", addTyping);      
+  //   };
+  // }, [addTyping, socket]);
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       <FormControl fullWidth hiddenLabel>
@@ -48,6 +89,7 @@ const Input = ({ otherUser, conversationId, user, postMessage }) => {
           placeholder="Type something..."
           value={text}
           name="text"
+          onKeyPress={handleKeyPress}
           onChange={handleChange}
         />
       </FormControl>
--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Box } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
+import { SocketContext } from '../../context/socket';
+
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
+  const [typing, setTyping] = useState(null);
+  const socket = useContext(SocketContext);
+  const addTyping = useCallback((data) => {
+    console.log(messages,"message",otherUser, userId);
+    if(data.recipientId===userId && data.senderId===otherUser.id){
+    setTyping("Typing");
+    }
+  }, []);
+  const stopTyping = useCallback((data) => {
+    setTyping("stopped");
+  }, []);
+  useEffect(() => {
+    // Socket init
+    socket.on("typing-success", addTyping);
+    socket.on("stop", stopTyping);
+    return () => {
+      socket.off("typing-success", addTyping);
+      socket.on("stop", stopTyping);
+    };
+  }, [addTyping, socket, stopTyping]);
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
-
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
@@ -22,6 +42,7 @@ const Messages = (props) => {
           />
         );
       })}
+      {(typing === "Typing") ? <OtherUserBubble otherUser={otherUser} time={""} text={typing} /> : null}
     </Box>
   );
 };
