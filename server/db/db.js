b26ca54cb456241254fe9f3607e51d34744c1678
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-03-07 12:49:38 -0500

    Add db

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,7 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/pi", {
-//   logging: false
-// });
-const db = new Sequelize('pi', 'postgres', 'a', {
-  host: 'localhost',
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/pi", {
+  logging: false
 });
+
 module.exports = db;
