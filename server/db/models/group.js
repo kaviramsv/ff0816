d33b6b888473da17b3bb24437b7c8c8c77be78fb
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-03-08 09:53:35 -0500

    Add db changes for including groups

---
+++ server/db/models/group.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Group = db.define("group", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }  
+});
+
+module.exports = Group;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,28 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Group = require("./group");
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsTo(User, { as: "user1",foreignKey: {
+  allowNull: true
+} });
+Conversation.belongsTo(User, { as: "user2" ,foreignKey: {
+  allowNull: true
+}});
+Conversation.belongsTo(Group, { as: "group" ,foreignKey: {
+  allowNull: true,
+} });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+User.belongsToMany(Group,{through:"user_group",as:"group",foreignKey: "user_id"});
+Group.belongsToMany(User,{through:"user_group",as:"user",foreignKey: "group_id"});
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Group
 };
