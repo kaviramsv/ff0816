b4d3f2684abec4cad3bc5d049ed3c5fec972c42b
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-03-10 09:01:57 -0500

    Db changes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,30 +2,24 @@ const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
 const Group = require("./group");
+const MessageStatus = require("./messageStatusgroup");
+//User group associations
 
-// associations
-
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1",foreignKey: {
-  allowNull: true
-} });
-Conversation.belongsTo(User, { as: "user2" ,foreignKey: {
-  allowNull: true
-}});
-Conversation.belongsTo(Group, { as: "group" ,foreignKey: {
-  allowNull: true,
-} });
-Group.belongsTo(User, { as: "owner" ,foreignKey: {
-  allowNull: false,
-} });
-Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
 User.belongsToMany(Group,{through:"user_group",as:"group",foreignKey: "user_id"});
 Group.belongsToMany(User,{through:"user_group",as:"user",foreignKey: "group_id"});
 
+//Message and  group associations
+Message.belongsTo(Group);
+Group.hasMany(Message);
+Message.belongsTo(User, { as: "sender" });
+
+Message.hasOne(MessageStatus);
+MessageStatus.belongsTo(Message, {as:"msg_id"});
+MessageStatus.belongsTo(User, {as:"otherUser_id"});
+
 module.exports = {
   User,
-  Conversation,
   Message,
-  Group
+  Group,
+  MessageStatus
 };
